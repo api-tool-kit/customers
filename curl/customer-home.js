9635ce4fe10219b9e1871f3e555053a2dfaec2b6
Mike Amundsen <mamund@yahoo.com>
2018-08-08 09:19:44 +0900

    add curl responses

---
+++ curl/customer-home.js
@@ -0,0 +1,28 @@
+{
+  "home": {
+    "actions": [
+      {
+        "name": "dashboard",
+        "prompt": "Home",
+        "href": "http://localhost:8282/",
+        "rel": [
+          "home",
+          "dashboard"
+        ],
+        "method": "GET"
+      },
+      {
+        "name": "customer",
+        "prompt": "Customers",
+        "href": "http://localhost:8282/customer/",
+        "rel": [
+          "collection",
+          "customer"
+        ],
+        "method": "GET"
+      }
+    ],
+    "data": []
+  },
+  "self": "http://localhost:8282"
+}
